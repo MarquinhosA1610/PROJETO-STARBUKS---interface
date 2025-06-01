d4af839c444ed20a43bb5a33a812231b29248094
MarquinosA1610 <marquinalexandre2005@gmail.com>
2025-06-01 01:28:20 -0300

    Troca da cor add

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,12 @@
 let IMG = document.querySelector(".IMG-P");
+let circulo = document.querySelector(".circulo")
+
 
 function TrocarIMG(endereco) {
     IMG.src = endereco
     
 };
+
+function TrocaCor(cor){
+    console.log(cor)
+}
